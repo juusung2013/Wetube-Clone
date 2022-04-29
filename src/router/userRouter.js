0b4f703a9f007af12debc83634c179fb59fc0d67
juusung2013 <juusung2013@gmail.com>
2022-04-29 20:19:33 +0900

    Learning Templates

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -5,8 +5,8 @@ import { edit, remove, see, logout } from "../controllers/userController";
 
 const userRouter = express.Router();
 
-userRouter.get(":id", see);
-userRouter.get("logout", logout);
+userRouter.get("/:id(\\d+)", see);
+userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/remove", remove);
 
